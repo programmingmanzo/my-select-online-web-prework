1da09b14fce2555818be42972730c6f2ea16f88d
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-05 06:26:05 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   array_select = []
   i = 0 
   while i < collection.length
-  yield(collection[i])          
+  array_select << yield(collection[i])          
   i += 1 
 end
 array_select
